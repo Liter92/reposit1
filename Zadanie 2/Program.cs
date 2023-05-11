346b5ca12f7013c9bbf4040e5f8bb327dcd221fe
Liter92 <125201317+Liter92@users.noreply.github.com>
2023-05-11 08:37:48 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int max = 0;
+        int min = 0;
+
+        Console.Write("Enter first number: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firstNumber > secondNumber)
+        {
+            max = firstNumber;
+            min = secondNumber;
+        }
+        else
+        {
+            max = secondNumber;
+            min = firstNumber;
+        }
+        Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+    }
+}
